021875e0f147215d124bbc1269ec59e9a7fd66bc
nakachan1994 <nakachaaaan@gmail.com>
2021-04-07 08:30:23 +0000

    [add]showコントローラー、ビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   end
 
   def destroy
-   
+
   end
 
   private
